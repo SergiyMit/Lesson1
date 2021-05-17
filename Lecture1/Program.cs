646e316526e22b264f8ed5d08f3594d270f6eba7
SergiyMit <84122888+SergiyMit@users.noreply.github.com>
2021-05-17 18:18:27 +0300

    Add files via upload

---
+++ Lecture1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using Nix1;
+namespace Lecture1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly asm = Assembly.LoadFrom("Lecture1Library.dll");
+            Console.WriteLine(asm.FullName);
+
+            Type[] types = asm.GetTypes();
+
+            foreach(Type t1 in types)
+            {
+                Console.WriteLine($"Name: {t1.Name} || Assembly: {t1.Assembly} || Typeinfo: {t1.GetTypeInfo()}");
+            }
+            Type t = asm.GetType("Lecture1Library.Diver", true, true);
+            object obj = Activator.CreateInstance(t);
+
+
+            MethodInfo method = t.GetMethod("Calculate");
+            object result = method.Invoke(obj, new object[] { 2, 2 });
+            Console.WriteLine(result);
+
+            Type type2 = typeof(User);
+            Console.WriteLine($"Name: {type2.Name}");
+            User user = new User("Sergiy", 20);
+            Console.ReadKey();
+
+        }
+    }
+}
